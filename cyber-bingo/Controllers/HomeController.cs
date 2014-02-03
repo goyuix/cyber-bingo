861ae51a827e4993f1b2b83996677da3c6d1e659
Goyuix <goyuix@gmail.com>
2014-02-03 11:24:25 -0700

    Initial checkin
    Basic functional version

---
+++ cyber-bingo/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cyber_bingo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                var random = new Random();
+                return RedirectToAction("Index", "Home", new { id = random.Next() });
+            }
+
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ cyber-bingo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cyber_bingo.MvcApplication" Language="C#" %>
